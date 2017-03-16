1e74dc9d221aafbcccadcc4911d53f40ee3645f5
Renan Vaz <renan.c.vaz@gmail.com>
2017-03-16 02:04:07 -0300

    Working...

---
+++ spec/pair-programming-z-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PairProgrammingZView from '../lib/pair-programming-z-view';
+
+describe('PairProgrammingZView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
